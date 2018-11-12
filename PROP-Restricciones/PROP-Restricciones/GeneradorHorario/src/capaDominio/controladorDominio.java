8bacdbc3ea7ebdb0772a18710eb950dd70fcf305
Victor Rojas <12918681+vrojasc@users.noreply.github.com>
2018-11-12 13:28:48 +0100

    Add files via upload
    Funciona backtracking

---
+++ PROP-Restricciones/PROP-Restricciones/GeneradorHorario/src/capaDominio/controladorDominio.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capaDominio;
+
+import capaDatos.Asignatura;
+//import capaDominio.FranjaHoraria.Dia;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author Victor
+ */
+public class controladorDominio {
+
+    private capaDatos.GestionDatos gestionDatos2;
+    private Restricciones rest;
+    private capaDominio.CjtAsignaciones CJTA;
+    
+ //   private CjtAsignaciones cjtAssigs;
+    public class FHaula{
+        private FranjaHoraria FH;
+        private capaDatos.Aula aula;
+        
+        public FHaula(FranjaHoraria FH, capaDatos.Aula aula){
+            this.FH = FH;
+            this.aula = aula;
+        }
+        public FranjaHoraria getFH(){
+            return FH;
+        }
+        public capaDatos.Aula getAula(){
+            return aula;
+        }
+    };
+    
+    public controladorDominio() {
+        System.out.println("CARGADO CONTROLADOR DOMINIO");
+        gestionDatos2 = new capaDatos.GestionDatos();
+        CJTA = new capaDominio.CjtAsignaciones();
+    }
+    
+    private FranjaHoraria.Dia Dia_semana(int dia){
+        
+        if (dia == 0) return FranjaHoraria.Dia.LUNES;
+        else if (dia == 1) return FranjaHoraria.Dia.MARTES;
+        else if (dia == 2) return FranjaHoraria.Dia.MIERCOLES;
+        else if (dia == 3) return FranjaHoraria.Dia.JUEVES;
+        else return FranjaHoraria.Dia.VIERNES;
+    }
+    private ArrayList<FHaula>  PosiblesFH(capaDatos.Asignatura A){
+        //Devuelve el conjunto de Franja horarias junto con las clases a las que podriamos meter la Asignatura A.
+        ArrayList<FHaula> ArrayFHaula = new ArrayList<>();
+        capaDatos.Aula aula = new capaDatos.Aula();
+        int HorasC = A.gethoraClase(); 
+        for (int diai = 0; diai < 5;++diai){
+            for (int horai = 8; (horai+HorasC)-1 <= 19;){
+                FranjaHoraria FH = new FranjaHoraria(horai,horai+HorasC-1, Dia_semana(diai));
+                Restricciones compRes = new Restricciones();
+                aula = compRes.Comprueba_Restricciones(A, FH, CJTA, gestionDatos2.getcjt_aules());
+                if (aula != null){//Funcion que dice si podria añadir la asignatura A en la FH.  
+                    FHaula fhaula = new FHaula(FH, aula);
+                    ArrayFHaula.add(fhaula);
+                    // Hay que añadir a que clase ira a parte de la FH. Supongo que creando un struck con FranjaHoraria y Aula ya estara.
+                }
+                //horai;
+                horai+=HorasC;
+            }
+        }
+        return ArrayFHaula;          
+    }
+  
+            
+    private boolean Generar_r(ArrayList<capaDatos.Asignatura> A, int i){
+        // i = numero de asignatura que estoy visitando en el array A.
+        //System.out.println(i);
+        if (i >= A.size()){
+            return true; // Todas las asignaturas han sido asignadas.
+        }
+        else{
+            boolean b = false;
+            capaDatos.Asignatura Aux = A.get(i++); // Cojo una asignatura. 
+            ArrayList<FHaula> ArrayFHaula = new ArrayList<>();
+            ArrayFHaula = PosiblesFH(Aux);// Para cada Asignatura me devuelve un array con la lista de franjas horarias donde la podria colocar junto con el Aula a la que iria..
+            int tam = 0;
+            if (ArrayFHaula != null) tam = ArrayFHaula.size();
+            //else System.out.println("Array vacio");
+            for (int j = 0; j < tam && !b; ++j){
+                Asignacion Asig = new Asignacion(ArrayFHaula.get(j).getAula(), Aux, ArrayFHaula.get(j).getFH()); // Primero crear asignacion 
+                                
+                CJTA.addelement(Asig);
+                b = Generar_r(A, i);// hacer recursividad
+                if (!b){
+                    CJTA.delelement();
+                }    // Eliminar Asignacion (ya que significara que por esta rama no ha acabado )
+                // Hay que controlar que no cree todas las ramas, por ejemplo haciendo que la funcion devuelva un bool, true cuando llegue al final y false mientras no.  
+                //if(!b && j==tam-1) System.out.println("No hay horario");
+            }
+             return b;   
+        }
+    }
+    public capaDominio.CjtAsignaciones Generar(){
+        ArrayList<capaDatos.Asignatura> A = gestionDatos2.getcjt_asignatures();
+        if(!Generar_r(A, 0)) System.out.println("no hay horario");
+        return CJTA;
+    }
+    
+    
+}
---
+++ PROP-Restricciones/PROP-Restricciones/GeneradorHorario/src/generadorhorario/GeneradorHorario.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+package generadorhorario;
+
+import capaDominio.Asignacion;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+
+/**
+ *
+ * @author Sergi
+ */
+public class GeneradorHorario {
+//Métodes d'aquesta clase han de ser static, ja que no hi ha cap instancia generada (de fet, tot amb una instancia ho podriem tractar aixi)
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        capaDominio.controladorDominio control = new capaDominio.controladorDominio();
+        
+        mostrar_horario_por_pantalla(control.Generar());
+        
+        
+        
+    }
+    
+    
+    private static void mostrar_horario_por_pantalla(capaDominio.CjtAsignaciones cjt){
+        Map<Integer, ArrayList<capaDominio.Asignacion>> m = cjt.get_asignaciones_ordenadas();
+        System.out.println("HORAS          LUNES          MARTES          MIERCOLES          JUEVES          VIERNES");
+        for (int hora = 0; hora < 12; hora++) {
+            System.out.print((hora+8) + ":00"); 
+            boolean acabat = false; int i = 0;
+            while (!acabat){
+                acabat = true;
+                for (int dia = 0; dia < 5; dia++){
+                    if (m.containsKey(dia*12 + hora) && m.get(dia*12 + hora).size() > i){
+                        System.out.print("          ");
+                        Asignacion a = m.get(dia*12 + hora).get(i);
+                        aux_mostrar_horario_por_pantalla(a);
+                        acabat = false;
+                    }
+                }
+                System.out.println("");
+                i++;
+            }
+        }
+    }
+    
+    
+    private static void aux_mostrar_horario_por_pantalla(Asignacion a){
+        System.out.print(a.getAsignatura().getMat().getSiglas() + " " + a.getAsignatura().getGrupo() + " " + a.getAsignatura().getTipusClase() + " [" + a.getAula().getCodigo() + "]");
+    }
+    
+}
