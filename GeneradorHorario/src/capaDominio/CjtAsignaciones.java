0609d9d8dc35c95d5ce4f255c3f366af4884eef5
Retrater12 <43724195+retrater12@users.noreply.github.com>
2018-12-16 17:38:30 +0100

    Add files via upload

--- GeneradorHorario/src/capaDominio/CjtAsignaciones.java
+++ GeneradorHorario/src/capaDominio/CjtAsignaciones.java
@@ -5,52 +5,178 @@
  */
 package capaDominio;
 
+import capaDatos.Asignatura;
+import static capaDatos.Asignatura.StringtoTipusclase;
+import static capaDatos.Asignatura.TipusClasetoString;
+import capaDatos.Aula;
 import capaDominio.Asignacion;
+import capaDominio.FranjaHoraria.*;
+import static capaDominio.FranjaHoraria.StringtoDia;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+ 
+
 /**
  *
- * @author Victor
+ * @author Sergi Aragall
  */
 public class CjtAsignaciones {
-
+/**
+ * Arraylist on es guarda totes les asignacions realitzades. Creades per la clase controladorDominio, per tal de construir l'horari.
+ */
     private ArrayList<Asignacion> cjt_asignaciones; 
-
+/**
+ * Constructora sense parametres de la clase CjtAsignacion.
+ */
     public CjtAsignaciones(){
         ArrayList<Asignacion> asig = new ArrayList<>();
         cjt_asignaciones = asig;
     }
+    /**
+     * Constructora amb parametres de la clase CjtAsignacion. Creara una clase CjtAsignacion a la cual se li asignara l'arraylist asig.
+     * @param asig Arraylist que se li asignara a la clase.
+     */
     public CjtAsignaciones(ArrayList<Asignacion> asig){
         cjt_asignaciones = asig;
     }
     
-    
+    /**
+     * Obte l'arraylist d'asignacions de la clase CjtAsignacions.
+     * @return Retorna l'arraylist d'asignacions.
+     */
     public ArrayList<Asignacion> getCjtA(){
         return cjt_asignaciones;
     }
+    /**
+     * Afegeix un element, en aquest cas una asignacio, a l'arraylist de la clase CjtAsignacions.
+     * @param A Asignacion que sera afegida al arraylist d'asignacions.
+     */
     public void addelement(Asignacion A){
-        
             cjt_asignaciones.add(A);
-
     }
+    /**
+     * Elimina l'ultim element de l'arraylist d'asignacions de la clase CjtAsignacions.
+     */
     public void delelement(){
         cjt_asignaciones.remove(cjt_asignaciones.size()-1);
     }
-    
+    /**
+     * Crea un map en que s'ordenen totes les asignacions que pertanyen a l'arraylist de la clase
+     * CjtAsignaciones segons el dia i l'hora de la franja horaria de cada una.
+     * @return Retorna un Map amb les asignacions ordenades segons hora i dia.
+     */
      public Map<Integer, ArrayList<Asignacion>> get_asignaciones_ordenadas (){
         Map<Integer, ArrayList<Asignacion>> m = new HashMap<Integer, ArrayList<Asignacion>>();  
         for (int i = 0; i < cjt_asignaciones.size(); i++){
             int val = cjt_asignaciones.get(i).getFranjaHoraria().unificar_values();
-            for (int j = 0; j < cjt_asignaciones.get(i).getAsignatura().gethoraClase(); j++){
+            for (int j = 0; j < cjt_asignaciones.get(i).getAsignatura().getHoraClase(); j++){
                 if (!m.containsKey(val+j))
                     m.put(val+j, new ArrayList<>());
                 m.get(val+j).add(cjt_asignaciones.get(i));      
             }
         }           
         return (HashMap<Integer, ArrayList<Asignacion>>) m;
-    }    
-    //pre: el conjunt assignacions ha estat montat
-    //post: es retorna un map ordenat amb les clases segons el dia i l'hora
+    }
+     
+    /**
+     *
+     * @param name
+     * @return
+     * @throws IOException
+     */     
+     public boolean guardar_horario(String name) throws IOException{
+         String ruta = "ArchivosExternos/horarios_guardados/Asignaciones/" + name + ".txt";
+         File file = new File(ruta);
+         BufferedWriter bw = null;
+         
+         if (file.exists())
+         {
+             return false; 
+         }
+         else
+         {
+            bw = new BufferedWriter(new FileWriter(ruta,false));
+             bw.write(name); // nombre del archivo
+             bw.newLine();
+             String siglas, grupo, tipoclase, codigo, dia, horaini, horafi;
+             
+             for (Asignacion asig : cjt_asignaciones){
+                 siglas = asig.getAsignatura().getMat().getSiglas();
+                 grupo = Integer.toString(asig.getAsignatura().getGrupo());
+                 tipoclase = TipusClasetoString(asig.getAsignatura().getTipusClase());
+                 codigo = asig.getAula().getCodigo();
+                 dia = asig.getFranjaHoraria().getDiaString();
+                 horaini = Integer.toString(asig.getFranjaHoraria().getHoraIni());
+                 horafi = Integer.toString(asig.getFranjaHoraria().getHoraFi());
+                 
+                 bw.write(siglas);bw.newLine();                         
+                 bw.write(grupo);bw.newLine();
+                 bw.write(tipoclase);bw.newLine();
+                 bw.write(codigo);bw.newLine();
+                 bw.write(dia);bw.newLine();
+                 bw.write(horaini);bw.newLine();
+                 bw.write(horafi);bw.newLine();
+               //  bw.write("!"+asig);bw.newLine();
+             }
+             
+             bw.close();
+             return true;
+         }
+     }
+     
+     
+     
+     public Asignacion crear(String siglas, String grupo, String tipoclase, String codigo, String dia, String horaini, String horafi, ArrayList<Asignatura> asignaturas, ArrayList<Aula> aulas){
+         Asignacion aux = new Asignacion();
+         boolean found = false;
+         for(Asignatura a : asignaturas){
+             if (a.getMat().getSiglas().equals(siglas) && Integer.toString(a.getGrupo()).equals(grupo) && a.getTipusClase() == StringtoTipusclase(tipoclase)){
+                 aux.setAsignatura(a);
+             }
+         }
+         for(Aula a : aulas){
+             if (a.getCodigo().equals(codigo)){
+                 aux.setAula(a);
+             }
+         }
+         FranjaHoraria fh = new FranjaHoraria(Integer.parseInt(horaini) , Integer.parseInt(horafi) , StringtoDia(dia));
+         aux.setFranjaHoraria(fh);
+         return aux;
+     }
+     
+     public boolean cargar_horario(String name, ArrayList<Asignatura> asignaturas, ArrayList<Aula> aulas) throws IOException{
+         String ruta = "ArchivosExternos/horarios_guardados/Asignaciones/" + name + ".txt";
+         File file = new File(ruta);         
+         if (!file.exists()) return false;
+         ArrayList<Asignacion> aux = new ArrayList<>();
+         BufferedReader br = new BufferedReader(new FileReader(ruta));
+         
+         String linia = br.readLine();
+         String siglas, grupo, codigo, dia, horaini, horafi, tipoclase;
+         while ((linia = br.readLine()) != null){
+             System.out.println(linia);
+             siglas = linia;    linia = br.readLine();System.out.println(linia);
+             grupo = linia;     linia = br.readLine();System.out.println(linia);
+             tipoclase = linia; linia = br.readLine();System.out.println(linia);
+             codigo = linia;    linia = br.readLine();System.out.println(linia);
+             dia = linia;       linia = br.readLine();System.out.println(linia);
+             horaini = linia;   linia = br.readLine();System.out.println(linia);
+             horafi = linia;
+             aux.add(crear(siglas, grupo, tipoclase, codigo, dia, horaini, horafi, asignaturas, aulas));
+             
+         }
+         cjt_asignaciones = aux;
+         return true;
+     }
 }
+
--- GeneradorHorario/src/capaDominio/controladorDominio.java
+++ GeneradorHorario/src/capaDominio/controladorDominio.java
@@ -13,13 +13,33 @@ import java.util.ArrayList;
 
 /**
  *
- * @author Victor
+ * @author Alberto Camacho
  */
 public class controladorDominio {
-
+/**
+ * GestionDatos amb totes les asignatures i les aules carregades.
+ */
     private capaDatos.GestionDatos gestionDatos2;
+    /*
+    Clase Restricciones que comprobara les restriccions a l'hora d'asignar aules a asignatures.
+    */
     private Restricciones rest;
+    /*
+    * Conjunt d'asignacions on es guardaran totes les asignacions que es van creant durant l'execucio per tal de guardar l'horari.
+    */
     private capaDominio.CjtAsignaciones CJTA;
+    
+    /**
+     * Constructora amb un parametre d'entrada.
+     * @param datos Correspon al nom del arxiu que es vol carregar.
+     */
+     public controladorDominio(String datos) {
+        gestionDatos2 = new capaDatos.GestionDatos("ArchivosExternos/" + datos);
+        CJTA = new capaDominio.CjtAsignaciones();
+        rest = new capaDominio.Restricciones();
+    }
+     
+     /* FUERA
     
  //   private CjtAsignaciones cjtAssigs;
     public class FHaula{
@@ -38,13 +58,9 @@ public class controladorDominio {
         }
     };
     
-    public controladorDominio() throws IOException {
-        System.out.println("CARGADO CONTROLADOR DOMINIO");
-        gestionDatos2 = new capaDatos.GestionDatos();
-        CJTA = new capaDominio.CjtAsignaciones();
-    }
+   
     
-    public FranjaHoraria.Dia Dia_semana(int dia){
+    private FranjaHoraria.Dia Dia_semana(int dia){
         
         if (dia == 0) return FranjaHoraria.Dia.LUNES;
         else if (dia == 1) return FranjaHoraria.Dia.MARTES;
@@ -56,58 +72,94 @@ public class controladorDominio {
         //Devuelve el conjunto de Franja horarias junto con las clases a las que podriamos meter la Asignatura A.
         ArrayList<FHaula> ArrayFHaula = new ArrayList<>();
         capaDatos.Aula aula = new capaDatos.Aula();
-        int HorasC = A.gethoraClase(); 
+        int HorasC = A.getHoraClase(); 
         for (int diai = 0; diai < 5;++diai){
-            for (int horai = 8; (horai+HorasC)-1 <= 19;){
+            for (int horai = 8; (horai+HorasC) <= 20;){
                 FranjaHoraria FH = new FranjaHoraria(horai,horai+HorasC-1, Dia_semana(diai));
-                Restricciones compRes = new Restricciones();
-                aula = compRes.Comprueba_Restricciones(A, FH, CJTA, gestionDatos2.getcjt_aules());
+                aula = rest.Comprueba_Restricciones(A, FH, CJTA, gestionDatos2.getcjt_aules());
                 if (aula != null){//Funcion que dice si podria añadir la asignatura A en la FH.  
                     FHaula fhaula = new FHaula(FH, aula);
                     ArrayFHaula.add(fhaula);
                     // Hay que añadir a que clase ira a parte de la FH. Supongo que creando un struck con FranjaHoraria y Aula ya estara.
                 }
                 //horai;
-                horai+=HorasC;
+            //    horai+=HorasC;
+            horai++;
+            
             }
         }
         return ArrayFHaula;          
     }
-  
-            
+  */
+       /**
+        * Funcio que genera recursivament una posible combinacio d'asignacions entre aules i asignatures. I les va guradan al CjtAsignaciones de la clase controladorDominio.
+        * @param A Arraylist que conte totes les asignatues que s'han d'asignar.
+        * @param i Variable per indicar a traves dels nivells de recursivitat quina de les asignatures estem tractan.
+        * @return Retorna true si hi existeix una posible combinacio i fals altrament.
+        */     
     private boolean Generar_r(ArrayList<capaDatos.Asignatura> A, int i){
-        // i = numero de asignatura que estoy visitando en el array A.
-        //System.out.println(i);
         if (i >= A.size()){
             return true; // Todas las asignaturas han sido asignadas.
         }
-        else{
-            boolean b = false;
+        else {
+            boolean b = false; boolean more_hours = true;
             capaDatos.Asignatura Aux = A.get(i++); // Cojo una asignatura. 
-            ArrayList<FHaula> ArrayFHaula = new ArrayList<>();
-            ArrayFHaula = PosiblesFH(Aux);// Para cada Asignatura me devuelve un array con la lista de franjas horarias donde la podria colocar junto con el Aula a la que iria..
-            int tam = 0;
-            if (ArrayFHaula != null) tam = ArrayFHaula.size();
-            //else System.out.println("Array vacio");
-            for (int j = 0; j < tam && !b; ++j){
-                Asignacion Asig = new Asignacion(ArrayFHaula.get(j).getAula(), Aux, ArrayFHaula.get(j).getFH()); // Primero crear asignacion 
-                                
-                CJTA.addelement(Asig);
-                b = Generar_r(A, i);// hacer recursividad
-                if (!b){
-                    CJTA.delelement();
-                }    // Eliminar Asignacion (ya que significara que por esta rama no ha acabado )
-                // Hay que controlar que no cree todas las ramas, por ejemplo haciendo que la funcion devuelva un bool, true cuando llegue al final y false mientras no.  
-                //if(!b && j==tam-1) System.out.println("No hay horario");
-            }
+            FranjaHoraria fh_aux = new FranjaHoraria(8, 8+Aux.getHoraClase()-1, FranjaHoraria.Dia.LUNES);
+        /* FUERA
+            //ArrayList<FHaula> ArrayFHaula = new ArrayList<>();
+            //ArrayFHaula = PosiblesFH(Aux);// Para cada Asignatura me devuelve un array con la lista de franjas horarias donde la podria colocar junto con el Aula a la que iria..
+            //int tam = 0;
+            //if (ArrayFHaula != null) tam = ArrayFHaula.size();
+            //else System.out.println("Array vacio"); 
+            */
+            do {
+                capaDatos.Aula aux_aula = null;
+                while (aux_aula == null && more_hours){
+                     aux_aula = rest.Comprueba_Restricciones(Aux, fh_aux, CJTA, gestionDatos2.getcjt_aules());
+                     if (aux_aula == null) more_hours = fh_aux.seguentHora();
+                }    
+                if (aux_aula != null) {
+                    Asignacion Asig = new Asignacion(aux_aula, Aux, fh_aux); // Primero crear asignacion 
+                    CJTA.addelement(Asig);
+                    b = Generar_r(A, i);// hacer recursividad    
+                    if (!b){
+                        CJTA.delelement();
+                        more_hours = fh_aux.seguentHora();
+                    }  
+                }                     
+            } while (!b && more_hours);
              return b;   
         }
     }
+    /**
+     * Funcio encarregada de cridar a la funcio recursiva de creacio de l'horari.
+     * @return Retorna un conjunt d'asignacions no vuit a no ser que no hi hagi combinacio posible.
+     */
     public capaDominio.CjtAsignaciones Generar(){
         ArrayList<capaDatos.Asignatura> A = gestionDatos2.getcjt_asignatures();
         if(!Generar_r(A, 0)) System.out.println("no hay horario");
         return CJTA;
     }
+    /**
+     * Obte el Conjunt d'asignacions de la clase controladorDominio.
+     * @return Retorna CJTA, el conjunt d'asignacions de la clase.
+     */
+    public capaDominio.CjtAsignaciones getCjtA(){
+        return CJTA;
+    }
     
     
+    public boolean guardar(String nombre, String datos) throws IOException{
+        if (CJTA.guardar_horario(nombre)){
+            return gestionDatos2.guardar_horario(nombre, datos);
+        }
+        return false;
+    }
+    
+    public boolean cargar(String nombre) throws IOException {
+        if (gestionDatos2.cargar_horario(nombre)){
+            return CJTA.cargar_horario(nombre, gestionDatos2.getcjt_asignatures(), gestionDatos2.getcjt_aules());
+        }
+        return false;
+    }
 }
